347219e3b1da9523b5635747457c0bca1d3636f0
Rathin Mondal <116378963+Rathin8670@users.noreply.github.com>
2024-06-30 00:51:13 +0530

    added routes in backend

---
+++ backend/routes/blog.ts
@@ -0,0 +1,117 @@
+import { Hono } from "hono";
+import { PrismaClient } from '@prisma/client/edge'
+import { withAccelerate } from '@prisma/extension-accelerate'
+import { verify } from 'hono/jwt'
+
+export const blogRouter=new Hono<{
+    Bindings:{
+        DATABASE_URL:string,
+        JWT_SECRET:string
+    },
+    Variables:{
+        userId:string
+    }
+}>();
+
+// middleware 
+blogRouter.use('/*',async (c,next)=>{
+	// get the header
+	// verify the header
+	// if the header is correct,we proceed
+	// if not return 403 
+	const header=c.req.header("authorization") ||""
+	const user=await verify(header,c.env.JWT_SECRET)
+	if(user){
+        //@ts-ignore
+        c.set("userId",user.id)
+		await next();
+	}else{
+		c.status(403);
+		return c.json({error:"unauthorized"})
+	}
+})
+
+blogRouter.post('/',async(c)=>{
+    const prisma = new PrismaClient({
+		datasourceUrl: c.env.DATABASE_URL,
+	}).$extends(withAccelerate())
+
+    const userId=c.get('userId')
+    const body=await c.req.json();
+    try{
+        const blog=await prisma.post.create({
+            data:{
+                title:body.title,
+                content:body.content,
+                authorId: userId
+
+            }
+        })
+        return c.json({
+            id:blog.id
+        })
+    }catch(e){
+        c.status(411);
+        return c.json({error:"error in post"})
+    }
+
+})
+
+blogRouter.put('/',async(c)=>{
+    const prisma = new PrismaClient({
+		datasourceUrl: c.env.DATABASE_URL,
+	}).$extends(withAccelerate())
+
+    const body=await c.req.json();
+    try{
+        const blog=await prisma.post.update({
+            where:{
+                id:body.id
+            },
+            data:{
+                title:body.title,
+                content:body.content,
+            }
+        })
+        return c.json({
+            id:blog.id
+        })
+    }catch(e){
+        c.status(417);
+        return c.json({error:"error in updating data"})
+    }
+})
+
+blogRouter.get('/',async(c)=>{
+    const prisma = new PrismaClient({
+		datasourceUrl: c.env.DATABASE_URL,
+	}).$extends(withAccelerate())
+
+    const body=await c.req.json();
+    try{
+        const blog=await prisma.post.findFirst({
+            where:{
+                id:body.id
+            }
+        })
+        return c.json({
+            blog
+        })
+    }catch(e){
+        c.status(417);
+        return c.json({error:"error in getting data"})
+    }
+})
+
+// pagination==>means see the first 10 or 15 posts which are capable in 1 page
+blogRouter.get('/bulk',async(c)=>{
+    const prisma = new PrismaClient({
+		datasourceUrl: c.env.DATABASE_URL,
+	}).$extends(withAccelerate())
+
+    const blogs=await prisma.post.findMany();
+
+    return c.json({
+        blogs
+    })
+})
